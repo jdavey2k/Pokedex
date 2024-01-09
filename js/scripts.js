482b7e3c13c3cafa1f5f8939528df148e52d8073
jdavey2k <154679812+jdavey2k@users.noreply.github.com>
2024-01-09 18:29:53 +0100

    CF 1.2 Update
    - Removed old JS
    - Added pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
-alert('Hello world');
-let favoriteFood = 'cheese';
-document.write(favoriteFood);
+let pokemonlist = []
+let pokemon = [{name: "gengar", types: ["ghost","poison"], weight: 0},
+{name: "Jynx", types: ["ice","psychic"], weight: 11},
+{name: "slowpoke", types: ["psychic","water"], weight: 6},]
