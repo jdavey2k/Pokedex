26f4dc2c7c9863ceb4af57a007de29068c73d550
jdavey2k <154679812+jdavey2k@users.noreply.github.com>
2024-01-10 11:11:37 +0100

    Update scripts.js
    Remove unneccecarry variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,3 @@
-let pokemonlist = []
-let pokemon = [{name: "gengar", types: ["ghost","poison"], weight: 0},
+let pokemonlist = [{name: "gengar", types: ["ghost","poison"], weight: 0},
 {name: "Jynx", types: ["ice","psychic"], weight: 11},
 {name: "slowpoke", types: ["psychic","water"], weight: 6},]
