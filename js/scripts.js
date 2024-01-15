233c10cf8997aed85fadf4c896380780dc51512e
jdavey2k <154679812+jdavey2k@users.noreply.github.com>
2024-01-15 11:25:04 +0100

    CF 1.3 Changes
    Added loop to iterate over the pokemon list
    
    Added if conditional to highlight tall
    
    pokemon within loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-let pokemonlist = [{name: "gengar", types: ["ghost","poison"], weight: 0},
-{name: "Jynx", types: ["ice","psychic"], weight: 11},
-{name: "slowpoke", types: ["psychic","water"], weight: 6},]
+let pokemonlist = [
+        { name: "gengar", types: ["ghost", "poison"], height: 104 },
+        { name: "Jynx", types: ["ice", "psychic"], height: 145 },
+        { name: "slowpoke", types: ["psychic", "water"], height: 42 },
+      ];
+      
+      for (let i = 0; i < pokemonlist.length; i++) {
+        let pokemon = pokemonlist[i];
+        document.write(pokemon.name + ", " + pokemon.height + "cm");
+      
+        if (pokemon.height > 130) {
+          document.write(" - Wow, that's big!");
+        }
+      
+        document.write("<br>");
+      }
